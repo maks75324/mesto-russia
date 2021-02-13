e8bdbe3c93e954a733e02c2e0949bb4a9f961fb1
skuratovich <maks75324@gmail.com>
2021-02-13 18:30:30 +0300

    js top

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,8 @@ let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
 let nameInput = formElement.querySelector('.popup__input_type_name');
 let jobInput = formElement.querySelector('.popup__input_type_job');
+let name = document.querySelector('.profile__heading');
+let job = document.querySelector('.profile__subheading');
 function openPopup () {
     popup.classList.add('popup_opened');
 }
@@ -19,9 +21,6 @@ function handleFormSubmit (evt) {
     let nameInputValue = nameInput.value;
     let jobInputValue = jobInput.value;
     // Получите значение полей из свойства value
-
-    let name = document.querySelector('.profile__heading');
-    let job = document.querySelector('.profile__subheading');
 
     name.textContent = nameInputValue;
     job.textContent = jobInputValue;
